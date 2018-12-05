f8834c7105e6d1f1a3e8176d8f1642416180687e
s17001 <s17001@std.it-college.ac.jp>
2018-12-05 21:43:23 +0900

    難易度選択を表示できるようにした

---
+++ app/src/main/java/jp/ac/itcollege/std/s17001/quiz/HardActivity.kt
@@ -0,0 +1,15 @@
+package jp.ac.itcollege.std.s17001.quiz
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class HardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
--- app/src/main/java/jp/ac/itcollege/std/s17001/quiz/MainActivity.kt
+++ app/src/main/java/jp/ac/itcollege/std/s17001/quiz/MainActivity.kt
@@ -1,12 +1,29 @@
 package jp.ac.itcollege.std.s17001.quiz
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        normal.setOnClickListener{
+            val dif_normal = Intent(this,NormalActivity::class.java)
+            startActivity(dif_normal)
+        }
+
+        hard.setOnClickListener{
+            val dif_hard = Intent(this,HardActivity::class.java)
+            startActivity(dif_hard)
+        }
+
+        no_order.setOnClickListener{
+            val nogenre = Intent(this,NogenreActivity::class.java)
+            startActivity(nogenre)
+        }
     }
 }
